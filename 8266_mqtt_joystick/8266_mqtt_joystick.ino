83b4d00c84b1ee76878eb2e17f1caa421d9313dc
gsalaman <43499190+gsalaman@users.noreply.github.com>
2019-10-17 16:16:44 -0600

    Beginnings of state framework and UI.

--- 8266_mqtt_joystick/8266_mqtt_joystick.ino
+++ 8266_mqtt_joystick/8266_mqtt_joystick.ino
@@ -13,7 +13,62 @@ IPAddress broker(10,0,0,17); // IP address of your MQTT broker eg. 192.168.1.50
 WiFiClient wclient;
 
 PubSubClient client(wclient); // Setup MQTT client
-bool state=0;
+
+
+typedef enum
+{
+  STATE_DISCONNECT = 0,
+  STATE_LOOKING_FOR_BROKER,
+  STATE_REGISTERING_WITH_GAME,
+  STATE_ACTIVE
+} state_type;
+
+void empty_serial_buffer( void )
+{
+  char c;
+
+  while (Serial.available())
+  {
+    c = Serial.read();
+  }
+}
+
+void print_top_menu( void )
+{
+  Serial.println("Configuration:");
+  Serial.println("1....set WiFi SSID and password");
+  Serial.println("2....set MQTT Broker");
+  Serial.println("3....set MQTT Client Name");
+}
+
+void user_input( void )
+{
+  char c;
+
+  while (Serial.available())
+  {
+    c = Serial.read();
+
+    switch (c)
+    {
+      case '1':
+        empty_serial_buffer();
+        configure_wifi();
+      break;
+
+      case '2':
+      break;
+
+      case '3':
+      break;
+
+      default:
+        print_top_menu();
+    }
+  }
+  
+}
+
 
 #define BUF_SIZE 40
 void configure_wifi( void )
@@ -21,6 +76,9 @@ void configure_wifi( void )
   char ssid[BUF_SIZE];
   char pass[BUF_SIZE];
   char *curr_char = ssid;
+
+  // disconnect from any currently assigned networks.
+  WiFi.disconnect();
   
   Serial.println("Enter SSID");
 
@@ -80,9 +138,30 @@ void configure_wifi( void )
   Serial.println(WiFi.localIP());
 }
 
+void init_disconnect_state( void )
+{
+  Serial.print("\nConnecting to network");
+  WiFi.begin(); // Connect to network, using last stored credentials.
+}
 
-// Connect to WiFi network
-void setup_wifi() {
+state_type process_disconnect_state( void )
+{
+    if (WiFi.status() != WL_CONNECTED) 
+    { 
+      // Wait for connection
+      delay(500);
+      Serial.print(".");
+      return STATE_DISCONNECT;
+    }
+    else
+    {
+      return STATE_LOOKING_FOR_BROKER;
+    }
+}
+
+// Connect to WiFi network using stored credentials.
+void setup_wifi() 
+{
   Serial.print("\nConnecting to network");
   
   //WiFi.begin(ssid, password); // Connect to network
@@ -125,17 +204,52 @@ void setup()
 
   Serial.println("Initializing 8266 MQTT Joystick");
 
+  delay(100);
+  init_disconnect_state();
+
+  /*
   configure_wifi();
-  
   pinMode(SWITCH_PIN,INPUT);  // Configure SWITCH_Pin as an input
   digitalWrite(SWITCH_PIN,HIGH);  // enable pull-up resistor (active low)
   delay(100);
   //setup_wifi(); // Connect to network
   client.setServer(broker, 1883);
+  */
+}
+
+void loop()
+{
+  static state_type current_state=STATE_DISCONNECT;
+  
+  // process any user input.  This will be a blocking call to configure the appropriate data.
+  user_input();  
+
+  switch (current_state)
+  {
+    case STATE_DISCONNECT:
+      current_state = process_disconnect_state();
+    break;
+
+    case STATE_LOOKING_FOR_BROKER:
+      Serial.println("You WIN!!!!");
+      delay(5000);
+    break;
+
+    case STATE_REGISTERING_WITH_GAME:
+    break;
+
+    case STATE_ACTIVE:
+    break;
+
+    default:
+      Serial.println("Unexpected STATE!!!");
+  }  // end of switch on current state
+    
 }
 
 
-void loop() {
+void old_loop() 
+{
   if (!client.connected())  // Reconnect if connection is lost
   {
     reconnect();
@@ -145,6 +259,7 @@ void loop() {
   // if the switch is being pressed
   if(digitalRead(SWITCH_PIN) == 0) 
   {
+    /*
     state = !state; //toggle state
     if(state == 1) // ON
     {
@@ -156,6 +271,7 @@ void loop() {
       client.publish(TOPIC, "off");
       Serial.println((String)TOPIC + " => off");
     }
+    */
 
     while(digitalRead(SWITCH_PIN) == 0) // Wait for switch to be released
     {
